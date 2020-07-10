984a86a0fbe43d7efd128d469b2090248eee69e6
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-07-10 21:55:11 +0530

    :shark: added learn-python repo notebooks (fixes #2)

---
+++ src/pages/lptc.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import getTableContent from '../utils/javascript/getTableContent';
+import TableContent from '../components/tableContent';
+import { graphql } from 'gatsby';
+
+const PythonPage = ({ data }) => {
+  const contents = data.allWantedBooks.edges.map(
+    (edge) => edge.node.fields.slug
+  );
+
+  const [table, paths] = getTableContent(contents);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <TableContent table={table} paths={paths} />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    allWantedBooks: allFile(
+      filter: {
+        extension: { eq: "ipynb" }
+        sourceInstanceName: { eq: "learn-python" }
+      }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default PythonPage;
