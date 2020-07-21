9651ce9a40fc83bfbe0e041f1824ad1c7aed87af
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-07-21 16:40:22 +0530

    :shark: About Section UI

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import Languages from './Languages';
+import About from './About';
 
 const UI = () => {
   return (
     <div id="shaurya-showcase">
       <Hero />
       <Languages />
+      <About />
     </div>
   );
 };
