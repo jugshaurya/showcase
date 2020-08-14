b57946aa4e230f5cc589abb0c5a86a01049efd34
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-08-14 14:18:57 +0530

    :snowflake: Added Snow Effect

---
+++ src/components/Snow.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import '../styles/snow.scss';
+
+const Snow = () => {
+  return (
+    <>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+      <div className="snow"></div>
+    </>
+  );
+};
+
+export default Snow;
